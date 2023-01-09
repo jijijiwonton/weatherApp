7c5cbb5879aea2010fb1006eb6af80b70bd417e7
jijijiwonton <ji04wonton30@gmail.com>
2023-01-09 16:32:46 -0500

    Create a function to display current location and fetch weather API url

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+/**
+ * 1. Display weather based on the current situation as soon as the app is running.
+ * 2. The weather info displays a city, celsius, fahrenheit.
+ * 3. Display 5 buttons (1 is for the current location, 4 are for different cities).
+ * 4. Each button allows to display appropriate city's weather.
+ * 5. While displaying data, loading spinner is showing.
+ */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Get current location using navigator object
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let latitude = position.coords.latitude;
+      let longitude = position.coords.longitude;
+
+      getWeatherAPIByGeoLocation(latitude, longitude);
+    });
+  };
+
+  // Get weather API based on a current location
+  const getWeatherAPIByGeoLocation = async (lat, lon) => {
+    const API = '7b5c92adb42c953e26d80267006836ad';
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`;
+    let response = await fetch(url);
+    let data = await response.json(response);
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+  return <div>Hello</div>;
 }
 
 export default App;
